ec425018a6b165edf85f8cc7062928da53a7d210
Dave Hampton <david.hampton@gmail.com>
2012-01-14 01:20:55 -0800

    initial tests and structure

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-module.exports = function(nodeApps, perlLibs) {
-
+var build;
+build = function(oObj) {
+  build.obj = oObj;
+  return build;
 }
+
+build.property = function(spec) {
+	var 
+      self = this,
+			value = spec.value || undefined,
+			name = spec.name,
+      oWritable = spec.hasOwnProperty("writable") ? spec.writable : false,
+      oEnumerable = spec.enumerable,
+      oConfigurable = spec.configurable;
+
+	Object.defineProperty(self.obj, name, {
+		get : function(){ return value; },  
+		set : function(newValue){
+      //Check for writability
+      oWritable && (value = newValue);
+      //even attempting to set a non writable object returns the attmepted value.  weird.
+      return newValue;
+     },  
+		enumerable : oEnumerable,  
+		configurable : oConfigurable
+	});  	
+	return true;
+};
+
+module.exports = build;
--- test/writable.js
+++ test/writable.js
@@ -1,13 +1,26 @@
 var test = require("tap").test,
-	perlReport = require('../lib/methods/lib/perlReport');
+	build = require('../index.js');
 
+//All atributes are Accessor Descriptors.  Writable and value are spoofed by modifying get and set
 test("methods", { skip:false }, function (t) {
-	t.plan(4);
+	t.plan(5);
 	t.ok(true, "true is ok -- all is right with the universe");
-	t.equal(typeof perlReport, 'function', "perlReport is a function");
-	function responseReceiver(error, result) {
-		t.equal(typeof result, 'object', "result is an object");
-		t.ok(result.version, "result.version is ok");
-	}
-	perlReport('/home/')(responseReceiver);
+
+	var obj = {};
+	build(obj).property({
+		name: "a",
+		value: 37,
+		writable: false
+	});
+  t.equal(obj.a, 37, 'Value was properly set and is retrievable');
+  obj.a = 38;
+  t.equal(obj.a, 37, 'Value was not changed when assigned');
+	build(obj).property({
+		name: "b",
+		value: 37,
+		writable: true
+	});
+  t.equal(obj.b, 37, 'Value was properly set and is retrievable');
+  obj.b = 38;
+  t.equal(obj.b, 38, 'Value was changed when assigned');
 });
